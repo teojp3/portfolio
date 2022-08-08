aa173a27bc6933e536b723146993c4fa17f1ac53
teojp3 <teojp3@gmail.com>
2022-08-08 14:23:28 +0800

    my portfolio website 08 Aug 2022

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
 app = Flask(__name__)
 print(__name__)
 
@@ -6,22 +8,30 @@ print(__name__)
 def main_page():
     return render_template('index.html')
 
-# @app.route('/index.html')
-# def home_button():
-#     return render_template('index.html')
-
-# @app.route('/works.html')
-# def works():
-#     return render_template('works.html')
-
-# @app.route('/about.html')
-# def about_me():
-#     return render_template('about.html')
-
-# @app.route('/contact.html')
-# def contact():
-#     return render_template('contact.html')
-
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         file = database.write(f'\n{email}, {subject}, {message}')
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thank-you.html')
+    else:
+        return 'something went wrong. Try again!'
